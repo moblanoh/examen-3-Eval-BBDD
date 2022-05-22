c971551bf5e751700d1289b0dd730f570ac7cec7
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-22 10:10:53 +0200

    Update 2.a.sql

--- 2.a.sql
+++ 2.a.sql
@@ -2,7 +2,7 @@ DROP FUNCTION IF EXISTS Nep;
 CREATE FUNCTION Nep(Cliente VARCHAR (5)) RETURN CHAR (2)
 BEGIN
 	SET @pais = (SELECT Cliente.Pais FROM Cliente WHERE Cliente.IdCliente=Cliente);
-	SET @resultado = IF (@pais = 'Francia','SI','NO');
+	SET @resultado = IF (@pais = 'España','SI','NO');
 	RETURN @resultado;
 END;
 
